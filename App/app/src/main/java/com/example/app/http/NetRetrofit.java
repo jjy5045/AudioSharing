c0ed0afb52bc00a8173fa2dcf8f2de8747fca811
jjy5045 <jjy0943@naver.com>
2022-03-18 16:38:10 +0900

    안드로이드 netretrofit 테스트

---
+++ App/app/src/main/java/com/example/app/http/NetRetrofit.java
@@ -0,0 +1,29 @@
+package com.example.app.http;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by NewLand on 2017. 11. 7..
+ */
+
+public class NetRetrofit {
+    private static NetRetrofit ourInstance = new NetRetrofit();
+    public static NetRetrofit getInstance() {
+        return ourInstance;
+    }
+    private NetRetrofit() {
+    }
+
+    Retrofit retrofit = new Retrofit.Builder()
+            //.baseUrl("http://10.0.2.2:8080/")
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create()) // 파싱등록
+            .build();
+
+    RetrofitService service = retrofit.create(RetrofitService.class);
+
+    public RetrofitService getService() {
+        return service;
+    }
+}
